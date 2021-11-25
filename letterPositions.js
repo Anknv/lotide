790e14f1a34dc026a3020cdfa86902f90a8b2fa0
Anknv <itsannakonovalova@gmail.com>
2021-11-25 01:58:34 +0000

    Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(arrOne, arrTwo) {
+  console.log(eqArrays(arrOne, arrTwo) ? '✅✅✅' : '❌❌❌');
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+const result1 = letterPositions("hello");
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3]);
+assertArraysEqual(result1["o"], [4]);
