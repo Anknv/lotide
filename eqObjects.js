c5a0ddff8465a5ba00f3b4521be859c9c0e511be
Anknv <itsannakonovalova@gmail.com>
2021-11-28 03:33:06 +0000

    add Arrays As Values

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
 const eqObjects = function(object1, object2) {
   if  (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
@@ -13,8 +27,17 @@ const eqObjects = function(object1, object2) {
     const keysInObj = Object.keys(object1);
     for (let key of keysInObj) {
       const value = object1[key];
-      if (value !== object2[key]) {
-        return false;
+      const value2 = object2[key];
+      if (Array.isArray(value)) {
+        //comparing array values
+        if (!eqArrays(value, value2)) {
+          return false;
+        }
+      } else {
+        //comparing primitive values
+        if (value !== value2) {
+          return false;
+        }
       }
     }
     return true;
@@ -33,3 +56,12 @@ const result1 = eqObjects(ab, abc);
 assertEqual(result, true);
 assertEqual(result1, false);
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const result2 = eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const result3 = eqObjects(cd, cd2); // => false
+
+assertEqual(result2, true);
+assertEqual(result3, false);
